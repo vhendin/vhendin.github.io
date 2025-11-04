19c61869b9ff56c315ca8470f7b1ab8b0cca9543
Magnus Vhendin <magnus.vhendin@gmail.com>
2025-11-04 18:28:46 +0100

    added uk countries

--- resources/scripts/world-creator.js
+++ resources/scripts/world-creator.js
@@ -14,31 +14,41 @@ const map = new maplibregl.Map({
 const tooltip = document.getElementById('tooltip');
 
 map.on('load', async function() {
-    const [countriesResponse, usStatesResponse] = await Promise.all([
+    const [countriesResponse, usStatesResponse, ukCountriesResponse] = await Promise.all([
         fetch('resources/countries.geojson'),
-        fetch('resources/us-states.geojson')
+        fetch('resources/us-states.geojson'),
+        fetch('resources/uk-countries.geojson')
     ]);
     
     const countriesData = await countriesResponse.json();
     const usStatesData = await usStatesResponse.json();
+    const ukCountriesData = await ukCountriesResponse.json();
     
-    const countriesWithoutUS = {
+    const countriesWithoutUSAndUK = {
         type: 'FeatureCollection',
         features: countriesData.features.filter(f => 
             f.properties.ADMIN !== 'United States of America' &&
-            f.properties.ISO_A3 !== 'USA'
+            f.properties.ISO_A3 !== 'USA' &&
+            f.properties.ADMIN !== 'United Kingdom' &&
+            f.properties.ISO_A3 !== 'GBR'
         )
     };
     
     map.addSource('countries', {
         type: 'geojson',
-        data: countriesWithoutUS,
+        data: countriesWithoutUSAndUK,
         generateId: true
     });
     
     map.addSource('us-states', {
         type: 'geojson',
         data: usStatesData,
+        generateId: true
+    });
+    
+    map.addSource('uk-countries', {
+        type: 'geojson',
+        data: ukCountriesData,
         generateId: true
     });
     
@@ -86,6 +96,31 @@ map.on('load', async function() {
         id: 'us-states-line',
         type: 'line',
         source: 'us-states',
+        paint: {
+            'line-color': '#fff',
+            'line-width': 1
+        }
+    });
+    
+    map.addLayer({
+        id: 'uk-countries-fill',
+        type: 'fill',
+        source: 'uk-countries',
+        paint: {
+            'fill-color': [
+                'case',
+                ['boolean', ['feature-state', 'hover'], false],
+                '#cccccc',
+                '#69b3a2'
+            ],
+            'fill-opacity': 1
+        }
+    });
+    
+    map.addLayer({
+        id: 'uk-countries-line',
+        type: 'line',
+        source: 'uk-countries',
         paint: {
             'line-color': '#fff',
             'line-width': 1
@@ -173,6 +208,48 @@ map.on('load', async function() {
         }
         
         hoveredStateId = null;
+        tooltip.style.opacity = '0';
+    });
+    
+    let hoveredUKId = null;
+    
+    map.on('mousemove', 'uk-countries-fill', function(e) {
+        if (e.features.length > 0) {
+            map.getCanvas().style.cursor = 'pointer';
+            
+            if (hoveredUKId !== null) {
+                map.setFeatureState(
+                    { source: 'uk-countries', id: hoveredUKId },
+                    { hover: false }
+                );
+            }
+            
+            hoveredUKId = e.features[0].id;
+            
+            map.setFeatureState(
+                { source: 'uk-countries', id: hoveredUKId },
+                { hover: true }
+            );
+            
+            const ukCountryName = e.features[0].properties.areanm;
+            tooltip.style.opacity = '1';
+            tooltip.innerHTML = ukCountryName;
+            tooltip.style.left = (e.point.x + 10) + 'px';
+            tooltip.style.top = (e.point.y - 10) + 'px';
+        }
+    });
+    
+    map.on('mouseleave', 'uk-countries-fill', function() {
+        map.getCanvas().style.cursor = '';
+        
+        if (hoveredUKId !== null) {
+            map.setFeatureState(
+                { source: 'uk-countries', id: hoveredUKId },
+                { hover: false }
+            );
+        }
+        
+        hoveredUKId = null;
         tooltip.style.opacity = '0';
     });
 });
