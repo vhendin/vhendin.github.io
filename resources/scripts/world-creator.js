2483ba1bff9becf35623e0b93b6ff597ee6c3b9a
Magnus Vhendin <magnus.vhendin@gmail.com>
2025-11-04 17:30:29 +0100

    Add initial implementation of World Map Creator with D3.js visualization and tooltip functionality

---
+++ resources/scripts/world-creator.js
@@ -0,0 +1,53 @@
+const width = window.innerWidth;
+const height = window.innerHeight;
+
+const svg = d3.select("#world-map")
+    .attr("width", width)
+    .attr("height", height);
+
+const projection = d3.geoNaturalEarth1()
+    .scale(width / 2 / Math.PI)
+    .translate([width / 2, height / 2])
+    .center([0, 0]);
+
+const path = d3.geoPath().projection(projection);
+
+const tooltip = d3.select("#tooltip");
+
+d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson").then(function(data) {
+    svg.append("g")
+        .selectAll("path")
+        .data(data.features)
+        .join("path")
+            .attr("class", "country")
+            .attr("fill", "#69b3a2")
+            .attr("d", path)
+            .attr("stroke", "#fff")
+            .attr("stroke-width", 0.5)
+            .on("mouseenter", function(event, d) {
+                d3.select(this)
+                    .attr("fill", "#2d7a5e");
+                
+                tooltip
+                    .style("opacity", 1)
+                    .html(d.properties.name)
+                    .style("left", (event.pageX + 10) + "px")
+                    .style("top", (event.pageY - 10) + "px");
+            })
+            .on("mousemove", function(event) {
+                tooltip
+                    .style("left", (event.pageX + 10) + "px")
+                    .style("top", (event.pageY - 10) + "px");
+            })
+            .on("mouseleave", function() {
+                d3.select(this)
+                    .attr("fill", "#69b3a2");
+                
+                tooltip
+                    .style("opacity", 0);
+            });
+});
+
+window.addEventListener('resize', function() {
+    location.reload();
+});
